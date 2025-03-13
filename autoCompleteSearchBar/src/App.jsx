efabb8418e2d6c8eb1edafa8a2023c41d530088d
varunpats <varunpatil2502@gmail.com>
2025-03-13 17:42:52 +0530

    Designed search bar and made API call on change

--- autoCompleteSearchBar/src/App.jsx
+++ autoCompleteSearchBar/src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState([]);
+  const [input, setInput] = useState('');
+
+  const fetchData = async () => {
+    const data = await fetch(`https://dummyjson.com/recipes/search?q=`);
+    const json = await data.json();
+    setResult(json.recipes);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [input])
 
   return (
-    <>
+    <div className='app'>
+      <h1>Autocomplete Searchbar</h1>
+      <input type='text' className='searchbar' value={input} onChange={(e) => { setInput(e.target.valueF) }} />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {result.map((res) => <span key={res.id}>{res.name}</span>)}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
