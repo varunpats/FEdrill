047d1899ee678022807ffc2f58aa5a8da6232e31
varunpats <varunpatil2502@gmail.com>
2025-03-15 22:49:49 +0530

    Implemented debouncing and caching.

--- autoCompleteSearchBar/src/App.jsx
+++ autoCompleteSearchBar/src/App.jsx
@@ -3,25 +3,34 @@ import './App.css'
 
 function App() {
   const [result, setResult] = useState([]);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
+  const [showResult, setShowResult] = useState(false);
+  const [cache, setCache] = useState({});
 
   const fetchData = async () => {
-    const data = await fetch(`https://dummyjson.com/recipes/search?q=`);
+    if(cache[input]) {
+      setResult(cache[input]);
+      return;
+    }
+    const data = await fetch(`https://dummyjson.com/recipes/search?q=${input}`);
     const json = await data.json();
     setResult(json.recipes);
+    setCache(prev => ({...prev, [input]: json?.recipes}))
   }
 
   useEffect(() => {
-    fetchData();
+    const timer = setTimeout(fetchData, 300);
+
+    return (() => { clearTimeout(timer) })
   }, [input])
 
   return (
     <div className='app'>
       <h1>Autocomplete Searchbar</h1>
-      <input type='text' className='searchbar' value={input} onChange={(e) => { setInput(e.target.valueF) }} />
-      <div>
-        {result.map((res) => <span key={res.id}>{res.name}</span>)}
-      </div>
+      <input type='text' className='searchbar' value={input || ""} onChange={(e) => { setInput(e.target.value) }} onFocus={() => setShowResult(true)} onBlur={() => setShowResult(false)} />
+      {showResult && <div className='result-container'>
+        {result.map((res) => <span className='result' key={res.id}>{res.name}</span>)}
+      </div>}
     </div>
   )
 }
